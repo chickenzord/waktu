428964f4e1e1bb869620a0c3ffd23404aeeb60bf
Akhyar Amarullah <akhyar.amarullah@gojek.com>
2021-09-24 14:59:33 +0700

    initial code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chickenzord/waktu
+
+go 1.16
+
+require github.com/tj/go-naturaldate v1.3.0 // indirect
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/tj/go-naturaldate"
+)
+
+func main() {
+	text := strings.Join(os.Args[1:], " ")
+	if text == "" {
+		os.Exit(1)
+	}
+
+	t, err := naturaldate.Parse(text, time.Now())
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("error parsing text: %s\n", err.Error()))
+		os.Exit(1)
+	}
+
+	fmt.Println(t.Format(time.RFC3339Nano))
+}
